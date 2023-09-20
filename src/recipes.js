703ca6c7b33036243c3323668b3cc52ff6f44634
Amanda Simonds <amandasimonds9@gmail.com>
2023-09-20 16:07:03 -0400

    random recipe appears

---
+++ src/recipes.js
@@ -0,0 +1,132 @@
+const recipes = [
+	{
+		"recipe_name": "Caprese Salad",
+		"ingredients": [
+			"Fresh mozzarella cheese",
+			"Tomatoes",
+			"Fresh basil",
+			"Balsamic glaze"
+		],
+		"instructions": [
+			"Slice the cheese and tomatoes.",
+			"Arrange them on a plate with fresh basil leaves.",
+			"Drizzle with balsamic glaze.",
+			"Season with salt and pepper to taste."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Avocado Toast",
+		"ingredients": ["Bread", "Ripe avocado", "Cherry tomatoes", "Salt"],
+		"instructions": [
+			"Toast the bread.",
+			"Spread mashed avocado on top.",
+			"Garnish with sliced cherry tomatoes.",
+			"Sprinkle with a pinch of salt."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Pasta Aglio e Olio",
+		"ingredients": [
+			"Spaghetti",
+			"Garlic cloves",
+			"Olive oil",
+			"Red pepper flakes (optional)"
+		],
+		"instructions": [
+			"Cook spaghetti according to package instructions.",
+			"Meanwhile, sauté minced garlic in olive oil until golden.",
+			"Toss cooked pasta with the garlic oil.",
+			"Add red pepper flakes for heat."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Greek Yogurt Parfait",
+		"ingredients": ["Greek yogurt", "Honey", "Granola", "Mixed berries"],
+		"instructions": [
+			"Layer Greek yogurt with honey, granola, and mixed berries in a glass or bowl.",
+			"Repeat for a delicious parfait."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Egg Fried Rice",
+		"ingredients": [
+			"Cooked rice",
+			"Eggs",
+			"Soy sauce",
+			"Frozen peas and carrots"
+		],
+		"instructions": [
+			"Scramble eggs in a pan.",
+			"Add cooked rice, frozen peas, and carrots.",
+			"Stir in soy sauce until heated through."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Chicken Quesadillas",
+		"ingredients": [
+			"Flour tortillas",
+			"Cooked chicken",
+			"Shredded cheese",
+			"Salsa"
+		],
+		"instructions": [
+			"Place chicken and cheese between tortillas.",
+			"Cook in a skillet until cheese is melted and tortillas are crispy.",
+			"Serve with salsa."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Cucumber and Cream Cheese Sandwiches",
+		"ingredients": ["Bread", "Cream cheese", "Cucumber slices", "Salt"],
+		"instructions": [
+			"Spread cream cheese on bread.",
+			"Add cucumber slices.",
+			"Sprinkle with a pinch of salt.",
+			"Assemble sandwiches."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Tomato Basil Pasta",
+		"ingredients": ["Pasta", "Cherry tomatoes", "Fresh basil", "Olive oil"],
+		"instructions": [
+			"Cook pasta.",
+			"Toss with halved cherry tomatoes, torn basil leaves, and a drizzle of olive oil.",
+			"Season with salt and pepper."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Tuna Salad",
+		"ingredients": ["Canned tuna", "Mayonnaise", "Celery", "Pickles"],
+		"instructions": [
+			"Mix drained tuna with mayo, diced celery, and chopped pickles.",
+			"Serve as a sandwich or on a bed of greens."
+		],
+		"time_minutes": 15
+	},
+	{
+		"recipe_name": "Mushroom and Spinach Quesadillas",
+		"ingredients": [
+			"Flour tortillas",
+			"Mushrooms",
+			"Spinach",
+			"Shredded cheese"
+		],
+		"instructions": [
+			"Sauté sliced mushrooms and spinach until wilted.",
+			"Place the mixture between tortillas with shredded cheese.",
+			"Cook until crispy."
+		],
+		"time_minutes": 15
+	}
+]
+
+
+export default recipes 
