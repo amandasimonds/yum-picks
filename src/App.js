9005808777a11ee689817ea45b3ae6bd405f69a6
Amanda Simonds <amandasimonds9@gmail.com>
2023-09-20 15:42:24 -0400

    Styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="flex justify-center box-shadow">
+				<h1>YumPicks</h1>
+			</header>
+			<div className="flex justify-center padding-md">
+				<button className="btn">Spin</button>
+			</div>
+			<div className='card'></div>
+		</div>
+	);
 }
 
 export default App;
